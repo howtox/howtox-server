1fc62e533e92125e1a2b65e770231f810dba52b8
Shaohua <sh.zhou@gmail.com>
2014-02-23 21:01:15 +0000

    WIP for creating container

--- routes/docker.js
+++ routes/docker.js
@@ -26,13 +26,32 @@ var resWrite = function(req, res, input){
 var Docker = require('dockerode');
 var docker = new Docker({socketPath: '/var/run/docker.sock'});
 
-exports.create = function(req, res){
-  console.log('docker',docker);
+var createContainer = function(){
+  docker.run('2947c9301082', [], process.stdout, function(err, data, container) {
+    console.log(data.StatusCode);
+  });
+};
+
+var listContainers = function(){
+  docker.listContainers(function(err, containers) {
+    console.log('list', err, containers);
+  });
+};
+
+var stopAll = function(){
   docker.listContainers(function(err, containers) {
     console.log('list', err, containers);
     containers.forEach(function(containerInfo) {
       docker.getContainer(containerInfo.Id).stop();
     });
   });
+};
+
+exports.create = function(req, res){
+  //not working because async
+  listContainers();
+  createContainer();
+  listContainers();
+
   resWrite(req, res, {key1: 'value1', key2: 'value2'});
 };
