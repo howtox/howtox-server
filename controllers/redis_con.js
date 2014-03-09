5b91ef5e2f676d2d5e5927d6df680a3e32181ab0
Shaohua <sh.zhou@gmail.com>
2014-03-09 03:49:21 +0000

    Tracking works

--- controllers/redis_con.js
+++ controllers/redis_con.js
@@ -9,21 +9,24 @@ redisCon.subClient.psubscribe('__keyevent@0__:expired');
 redisCon.register = function(input){
   redisCon.pubClient.set(input, 'randomestring', 'EX', 900, redis.print);
   analytics.track({
+    userId: 'null',
     containerId: input,
-    status: 'start',
+    event: 'start',
     time: new Date()
   });
+  console.log('redis register');
 };
 
 redisCon.stopCallback = function(cb){
   redisCon.subClient.on('pmessage', function(pattern, channel, expiredKey){
     console.log('key expired: ', expiredKey);
     analytics.track({
+      userId: 'null',
       containerId: expiredKey,
-      status: 'stop',
+      event: 'stop',
       time: new Date()
     });    
-    
+    console.log('redis stop');    
     cb(expiredKey);
   });
 };
