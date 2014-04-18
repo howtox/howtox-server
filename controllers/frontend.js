74630f0c29aad042b906c8388845769bd02d9e25
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-17 21:48:22 -0700

    Works, but ugly

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -3,3 +3,15 @@ var frontendCon = module.exports = {};
 frontendCon.index = function(req, res){
   res.end('index');
 };
+
+frontendCon.getBox = function(req, res){
+  var boxId = req && req.params && req.params.id;
+  var domain = 'http://192.241.231.60:';
+  var editorSrc = domain + boxId;
+  var terminalSrc = domain + (parseInt(boxId, 10) + 2);
+  // res.end('index' + boxId);
+  res.render('box_iframe', {
+    editorSrc: editorSrc,
+    terminalSrc: terminalSrc
+  });
+};
