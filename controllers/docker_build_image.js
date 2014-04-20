66ceab8d0d2cae585f3f7847f46fdc623d68dd7f
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-20 15:39:47 -0700

    Docker build in /temp

--- controllers/docker_build_image.js
+++ controllers/docker_build_image.js
@@ -1,12 +1,15 @@
 var _ = require('underscore'),
   pexec = require('../utils/command_line').pexec,
+  path = require('path'),
   templates = require('./docker_templates');
 
 var controllers = module.exports = {};
 
 var build = function(imageName){
   console.log('build', imageName);
-  pexec('docker build .');
+  pexec('docker build .', {
+    cwd: path.join(__dirname, '..' ,'/temp')
+  });
 };
 
 controllers.buildImage = function(imageName){
