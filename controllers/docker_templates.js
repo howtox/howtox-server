839de0341c1fe2a9f7c66574ac63c93b9c98dd00
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-19 17:40:50 -0700

    Templating roughly works

--- controllers/docker_templates.js
+++ controllers/docker_templates.js
@@ -8,8 +8,18 @@ Templates.node = function(){
   var absPath = path.join(__dirname, '../docker_templates') + '/medium.txt';
   var medium = fs.readFileSync(absPath, {encoding: 'utf8'});
   var mediumTemplate = Handlebars.compile(medium);
+  var preBuildHook = 'RUN easy_install supervisor \n' +
+                     'RUN mkdir -p /var/log/supervisor';
+  var postBuildHook = 'RUN easy_install supervisor \n' +
+                     'RUN mkdir -p /var/log/supervisor';
+
   var output = mediumTemplate({
-    preBuildHook: 'test'
+    preBuildHook: preBuildHook,
+    postBuildHook: postBuildHook
   });
+
+  var absOutputPath = path.join(__dirname, '../temp');
+  fs.writeFileSync(absOutputPath + '/test', output, {encoding: 'utf8'});
+
   console.log('medium', output);
 };
