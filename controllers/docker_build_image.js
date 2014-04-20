0a60168b7a2facea6df4ab3f81b37eba62e90d1e
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-19 17:54:24 -0700

    Add build

--- controllers/docker_build_image.js
+++ controllers/docker_build_image.js
@@ -1,8 +1,14 @@
 var _ = require('underscore'),
+  pexec = require('../utils/command_line').pexec,
   templates = require('./docker_templates');
 
 var DockerCon = module.exports = {};
 
+var build = function(){
+  pexec('docker build .');
+};
+
 DockerCon.buildImage = function(imageName){
   templates.node();
+  build();
 };
