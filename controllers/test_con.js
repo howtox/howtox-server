677a97d58c7300b11c8ce4b03dce72554918e074
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-20 15:24:42 -0700

    Add test controller

---
+++ controllers/test_con.js
@@ -0,0 +1,5 @@
+var controllers = module.export = {};
+
+controllers.test = function(){
+  console.log('test');
+};
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,7 +1,9 @@
 var frontend    = require('../controllers/frontend');
+var testCon    = require('../controllers/test_con');
 
 module.exports = function (app) {
   app.get('/', frontend.index);
   app.get('/box/:id', frontend.getBox);
   app.get('/launch/:githubUser/:githubRepo', frontend.launchRepo);
+  app.get('/test', testCon.test);
 };
