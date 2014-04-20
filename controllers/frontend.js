1cbddab0e39f27e283b48efbd2758e0811d48015
Shaohua Zhou <sh.zhou@gmail.com>
2014-04-20 14:37:17 -0700

    change domain

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -11,7 +11,7 @@ frontendCon.index = function(req, res){
 
 frontendCon.getBox = function(req, res){
   var boxId = req && req.params && req.params.id;
-  var domain = 'http://192.241.231.60:';
+  var domain = 'http://da.howtox.com:';
   var editorSrc = domain + boxId;
   var terminalSrc = domain + (parseInt(boxId, 10) + 2);
   // res.end('index' + boxId);
