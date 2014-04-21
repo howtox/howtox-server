86373067deab7eec5c4a1a7fec8373f8d1db5258
Shaohua <sh.zhou@gmail.com>
2014-04-21 03:39:17 +0000

    Add socket.io

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ require('./config/db.js')(app);
 require('./config/middleware.js')(app);
 require('./routes')(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port') + ' in env: ' + app.get('env') );
+});
+
+var io = require("socket.io").listen(server)
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
 });
