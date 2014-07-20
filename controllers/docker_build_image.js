7a79747eabff89382a7faf16bf1fb45838ac5158
Shaohua <sh.zhou@gmail.com>
2014-07-20 02:34:48 -0400

    Add cmd tool

--- controllers/docker_build_image.js
+++ controllers/docker_build_image.js
@@ -1,5 +1,7 @@
 var pexec = require('../utils/command_line').pexec,
   path = require('path'),
+  Q = require('q'),
+  spawn = require('child_process').spawn,
   prepareTemplates = require('./docker_prepare_templates');
 
 var controllers = module.exports = {};
@@ -13,6 +15,30 @@ var build = function(options){
   });
 };
 
+var buildSpawn = function(options){
+  console.log('buildSpawn', options);
+  var dfd = Q.defer();
+
+  var imageName = options.userName + '/' + options.repoName;
+
+  //http://stackoverflow.com/questions/20357216/stream-stdout-from-child-process-to-browser-via-expressjs
+  var child = spawn('docker', ['build', '-t', imageName, '.'],  {
+    cwd: path.join(__dirname, '..' ,'/temp', options.repoName)
+  });
+  //http://rockycode.com/blog/pipe-stdout-socketio/
+  child.stdout.setEncoding('utf-8');
+
+  child.stdout.on('data', function(chunk){
+    console.log(chunk);
+  });
+  child.stdout.on('end', function(){
+    console.log('stdout:end');
+    dfd.resolve();
+  });
+
+  return dfd.promise;
+};
+
 controllers.buildImage = function(imageName){
   var options = {
     userName: imageName.split('/')[0],
@@ -20,4 +46,13 @@ controllers.buildImage = function(imageName){
   };
   prepareTemplates.completeRegenerate(options);  //sync
   return build(options);
+};
+
+controllers.buildImageSpawn = function(imageName){
+  var options = {
+    userName: imageName.split('/')[0],
+    repoName: imageName.split('/')[1]
+  };
+  prepareTemplates.completeRegenerate(options);  //sync
+  return buildSpawn(options);
 };
